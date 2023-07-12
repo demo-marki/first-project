e1d3763be6815465bf8becb8941079618040b8eb
irina.markova <irina.markova@rubius.com>
2023-07-12 18:43:01 +0700

    Поправила версию

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import {Route, Routes} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//function App() {
+const App = () => {
+    return (
+            <div className='app-wrapper'>
+                <HeaderContainer />
+                <Navbar />
+                <div class='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/profile/:userId?' element={<ProfileContainer />}/>
+                        <Route path='/dialogs/*' element={<DialogsContainer />}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                        <Route path='/users' element={<UsersContainer />}/>
+                        <Route path='/login' element={<Login />}/>
+                    </Routes>
+                </div>
+            </div>
+    );
 }
 
 export default App;
